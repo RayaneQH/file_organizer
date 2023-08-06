b05edc2df4273f2df83df437bc44adf0771c8a14
RayaneLaptop <Rayane@Laptop>
2023-08-06 11:58:23 +0100

    add some functinality to the fine_deal_duplicate_files.py

--- helper_functions/find_deal_duplicate_files.py
+++ helper_functions/find_deal_duplicate_files.py
@@ -1,28 +1,30 @@
-import os
-from pathlib import Path
-from filecmp import cmp
-  
-  
-DATA_DIR = Path()
-files = sorted(os.listdir(DATA_DIR))
-  
-duplicateFiles = []
-  
-for file_x in files:
-  
-    if_dupl = False
-  
-    for class_ in duplicateFiles:
-        if_dupl = cmp(
-            DATA_DIR / file_x,
-            DATA_DIR / class_[0],
-            shallow=False
-        )
-        if if_dupl:
-            class_.append(file_x)
-            break
-  
-    if not if_dupl:
-        duplicateFiles.append([file_x])
-  
-print(duplicateFiles)    
+def make_set_of_files(files):
+    set_of_files = set()
+    for i in files:
+        set_of_files.add(i[0])
+    return set_of_files
+
+
+def duplicate(path):
+    import os
+    from pathlib import Path
+    from filecmp import cmp
+
+    DATA_DIR = Path(path)
+    files = sorted(os.listdir(DATA_DIR))
+
+    duplicateFiles = []
+
+    for file_x in files:
+        if_dupl = False
+
+        for class_ in duplicateFiles:
+            if_dupl = cmp(DATA_DIR / file_x, DATA_DIR / class_[0], shallow=False)
+            if if_dupl:
+                class_.append(file_x)
+                break
+
+        if not if_dupl:
+            duplicateFiles.append([file_x])
+    set_of_files = make_set_of_files(duplicateFiles)
+    return set_of_files
